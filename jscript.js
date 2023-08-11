0af679f764fc2e371b3bc307c6afa91cb59b7380
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-08-11 14:13:41 -0400

    deleted extra javascript file

--- jscript.js
+++ jscript.js
@@ -1,1 +1,9 @@
-function
+function getRecipe (){
+    fetch(`https://api.api-ninjas.com/v1/recipe?query=asparagus`)
+    .then(resp => {
+        return resp.json
+    })
+    .then(Recipe =>{
+        console.log(Recipe)
+    })
+}
