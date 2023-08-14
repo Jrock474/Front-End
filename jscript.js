babb8a792a7ec0d64b3123bd4c7e9f83dbbfc4b0
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-08-14 10:21:22 -0400

    going to add media query

--- jscript.js
+++ jscript.js
@@ -1,4 +1,4 @@
-let url = 'https://api.api-ninjas.com/v1/recipe?query=pork'
+let url = 'https://api.api-ninjas.com/v1/recipe?query=broccoli'
 let key = {
     headers: { 'x-api-key': '9uIr2fvAn8JOJkk8M5TFFV4EUTHMNNtuXdcsIQkj' }
   }
@@ -8,3 +8,5 @@ let key = {
         .then(recipes => {
           console.log(recipes)
         })
+
+
