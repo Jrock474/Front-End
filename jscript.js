8280e6cdd076cacf78e98b6aed05bf8b3ab69c6a
COR Coding <corcoding@Coding-9.localdomain>
2023-08-14 15:44:21 -0400

    text

--- jscript.js
+++ jscript.js
@@ -1,25 +1,28 @@
-
 const submitQuery = document.getElementById("api-query");
 const userInput = document.getElementById("query-input");
-
-const fetchData = ()=>{
-    const inputValue = userInput.value; // Retrieve the input value when the button is clicked
-    console.log("user input", inputValue);
-
-    let url = `https://api.api-ninjas.com/v1/recipe?query=${inputValue}`;
-    let key = {
+const recipesDiv = document.getElementById("recipe-list");
+const fetchData = () => {
+    const inputValue = userInput.value;
+    const url = `https://api.api-ninjas.com/v1/recipe?query=${inputValue}`;
+    const key = {
         headers: { 'x-api-key': '9uIr2fvAn8JOJkk8M5TFFV4EUTHMNNtuXdcsIQkj' }
     };
-
     fetch(url, key)
-        .then(res => res.json())
-        .then(recipes => {
-            console.log(recipes);
+    .then(res => res.json())
+    .then(recipes => {
+        console.log('Fetched data:', recipes);
+        recipes.results.forEach(recipe => {
+            const recipeItem = document.createElement('div');
+            const titleElement = document.createElement('p');
+            titleElement.innerText = recipe.title;
+            recipeItem.appendChild(titleElement);
+            recipesDiv.appendChild(recipeItem);
         });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 }
-
-
-
 submitQuery.addEventListener("click", () => {
-    fetchData()
+    fetchData();
 });
