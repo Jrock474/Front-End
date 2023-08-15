a1fff09e8f1931b1d682f140f38f25f2e232c8e8
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-08-15 10:11:17 -0400

    added a second API, re ordered the API, added more text to my sections

--- jscript.js
+++ jscript.js
@@ -1,3 +1,48 @@
+// EXERCISE API GENERATOR
+
+const submitQuery2 = document.getElementById("api-query2");
+const userInput2 = document.getElementById("query-input2");
+const exercisesDiv = document.getElementById("exercise-list");
+
+const fetchData2 = () => {
+    const inputValue2 = userInput2.value; 
+    const url = `https://api.api-ninjas.com/v1/exercises?muscle=${inputValue2}`; 
+    const key = {
+        headers: { 'x-api-key': '9uIr2fvAn8JOJkk8M5TFFV4EUTHMNNtuXdcsIQkj' }
+    };
+
+    exercisesDiv.innerHTML = "";
+    fetch(url, key)
+    .then(res => res.json())
+    .then(exercises => {
+        console.log('Fetched data:', exercises);
+        exercises.forEach(exercise => {
+            const exerciseItem = document.createElement('div');
+            const titleElement = document.createElement('p');
+            const instructionsList = document.createElement('p')
+            instructionsList.innerText = exercise.difficulty;
+            titleElement.innerText = exercise.name;
+            exerciseItem.innerText = exercise.instructions; 
+            
+            
+            exerciseItem.appendChild(instructionsList)
+            exerciseItem.appendChild(titleElement);
+            exercisesDiv.appendChild(exerciseItem);
+            
+            exercisesDiv.style.border = 'solid 10px black';
+            exercisesDiv.style.padding = '10px';
+        })
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+submitQuery2.addEventListener("click", () => { 
+    fetchData2();
+});
+
+
+// Recipe API
 const submitQuery = document.getElementById("api-query");
 const userInput = document.getElementById("query-input");
 const recipesDiv = document.getElementById("recipe-list");
