c2f3401bef3e0bae730796471b0d6d7bffcce560
Azure_Moon <jordanw0412@gmail.com>
2023-08-14 22:13:40 -0400

    deleted anime packages

--- jscript.js
+++ jscript.js
@@ -27,11 +27,3 @@ submitQuery.addEventListener("click", () => {
     fetchData();
 });
 
-anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-  });
-
