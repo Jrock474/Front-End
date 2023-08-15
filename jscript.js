8562b4fdc12a82d0db50eca9f74e1fd131c5662e
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-08-15 09:04:29 -0400

    added exercise api to generate information based on user input

--- jscript.js
+++ jscript.js
@@ -29,3 +29,36 @@ submitQuery.addEventListener("click", () => {
     fetchData();
 });
 
+// API FOR EXERCISES
+
+const submitQuery2 = document.getElementById("api-query2");
+const userInput2 = document.getElementById("query-input2");
+const exercisesDiv = document.getElementById("exercise-list");
+
+const fetchData2 = () => {
+    const inputValue2 = userInput2.value; 
+    const url = `https://api.api-ninjas.com/v1/exercises?muscle=${inputValue2}`; 
+    const key = {
+        headers: { 'x-api-key': '9uIr2fvAn8JOJkk8M5TFFV4EUTHMNNtuXdcsIQkj' }
+    };
+    fetch(url, key)
+    .then(res => res.json())
+    .then(exercises => {
+        console.log('Fetched data:', exercises);
+        exercises.forEach(exercise => {
+            const exerciseItem = document.createElement('div');
+            const titleElement = document.createElement('p');
+            titleElement.innerText = exercise.title;
+            exerciseItem.innerText = exercise.instructions; 
+            exercisesDiv.appendChild(exerciseItem);
+            exerciseItem.appendChild(titleElement);
+        })
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+submitQuery2.addEventListener("click", () => { 
+    fetchData2();
+});
+
